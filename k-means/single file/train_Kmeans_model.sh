30b8d145002a84469b8f75773077b2f7f615bf08
jhtcwyk <18817867753@163.com>
2017-04-22 10:18:05 -0400

    filter data

--- k-means/single file/train_Kmeans_model.sh
+++
@@ -1,3 +0,0 @@
-/opt/maven/bin/mvn clean
-/opt/maven/bin/mvn package 
-nohup spark-submit --packages com.databricks:spark-csv_2.11:1.1.0 --name "K-means" --class Kmeans --master yarn target/KmeansModel-0.0.1.jar &
